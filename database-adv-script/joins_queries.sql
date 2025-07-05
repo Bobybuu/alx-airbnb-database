e8d6585daba8de7bff63209d1c79be4205af1cd9
Bobybuu <c43057772@gmail.com>
2025-07-06 00:43:00 +0300

    Added joins_queries.sql in  database-adv-script -a directory to Master SQL joins by writing complex queries using different types of joins  for AirBNB Database

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,70 @@
+/*Write a query using an INNER JOIN 
+to retrieve all bookings and the respective 
+users who made those bookings.
+*/
+
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date,
+    bookings.total_price,
+    bookings.status,
+    bookings.created_at AS booking_created_at,
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    users.email
+FROM bookings
+INNER JOIN users
+    ON bookings.user_id = users.user_id;
+
+/*Write a query using aLEFT JOIN 
+to retrieve all properties and their reviews, 
+including properties that have no reviews.
+*/
+
+SELECT 
+    properties.property_id,
+    properties.name AS property_name,
+    properties.location,
+    properties.pricepernight,
+    reviews.review_id,
+    reviews.rating,
+    reviews.comment,
+    reviews.created_at AS review_date,
+    reviews.user_id AS reviewer_id
+FROM properties
+LEFT JOIN reviews
+    ON properties.property_id = reviews.property_id;
+
+-- Simulated FULL OUTER JOIN: Users ↔ Bookings
+SELECT 
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    users.email,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date,
+    bookings.total_price,
+    bookings.status
+FROM users
+LEFT JOIN bookings ON users.user_id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    users.email,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date,
+    bookings.total_price,
+    bookings.status
+FROM bookings
+RIGHT JOIN users ON users.user_id = bookings.user_id;
